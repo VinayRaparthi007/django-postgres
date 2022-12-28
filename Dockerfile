f289d99903283db509a5df306dc6de63f1ac4c6f
vinay <raparthivinay99@gmail.com>
2022-12-28 11:42:07 +0530

    Postgres

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,10 @@
 FROM python:3.8
-
 ENV PYTHONUNBUFFERED=1
-
-RUN apt-get update 
-
+RUN apt update 
 WORKDIR /usr/scr/app
-
 COPY req.txt ./
-
 RUN pip install -r req.txt
-
 COPY . .
-
 EXPOSE 8000
-
 #CMD ["python","manage.py","runserver","0.0.0.0:8000"]
 
--- myapp/settings.py
+++ myapp/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'postgres'),
+        'USER': 'root',
+        'PASSWORD': 'postgres',
+        'HOST': 'db_postgres',
+        'PORT': 5432
+
     }
 }
 
